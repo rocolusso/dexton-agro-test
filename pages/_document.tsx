ed6ea76ab0a675b8ce66daa0731af9ac6b24ffe5
ysphotodev <ysphotodev@gmail.com>
2024-07-30 23:50:11 +0300

    Changed title changed favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+        <Head>
+            <title>DEXTON HANDEL GMBH</title>
+            <link rel="icon" type="image/x-icon" href="/logo_fav.ico"/>
+        </Head>
+        <body>
+        <Main/>
+        <NextScript/>
+        </body>
     </Html>
   );
 }
